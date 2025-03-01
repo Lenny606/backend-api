debe4353a6e0558d2c6eb99c10db0f5228a1c7f9
Tomas Kravcik <kravcik@procorp.cz>
2025-03-01 15:54:53 +0100

    push bad code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import {connectDB} from "./database/db.js";
 import {errorMiddleware} from "./middleware/error.middleware.js";
 
 const app = express()
+//BAD CODE !!!!!!!!!!!!!!!
 
 //MW
 app.use(express.json())
