d77b00b435aae9953bf69634116ffd80b816f25e
Tomas Kravcik <kravcik@procorp.cz>
2025-03-02 19:14:21 +0100

    is admin func

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -33,3 +33,18 @@ export const authMiddleware = async (err, req, res, next) => {
     }
 
 }
+
+export const isAdmin = async (req, res, next) => {
+    try {
+        if (req.user && req.user.role === 'admin') {
+            next();
+        } else {
+            return res.status(403).json({
+                success: false,
+                message: "Access denied. Admin privileges required."
+            });
+        }
+    } catch (err) {
+        next(err);
+    }
+}
